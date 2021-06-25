b479852519b8558d1d461e402d17542e81e73f3e
ricardobjr <ricardo-jr-1@live.com>
2021-06-25 10:51:10 -0300

    teste com hello world

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -8,11 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class BootcampApplication {
-
+	/*
 	@Bean
 	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
 		return new OpenAPI().info( new Info()
@@ -20,6 +22,13 @@ public class BootcampApplication {
 				.version("1.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+	 */
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Hello World";
 	}
 
 	public static void main(String[] args) {
